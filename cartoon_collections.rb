11d16bae528abf77946d38edfe2e8ad8cb15dea1
Collin Jilbert <cjilbert504@gmail.com>
2019-11-12 16:37:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(array)
   array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)# code an argument here
+  array.detect {|i| i.is_a?(String)}# the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
