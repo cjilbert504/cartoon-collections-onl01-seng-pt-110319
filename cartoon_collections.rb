ce4bcd9152760605bac6a8f288b05f5db8858b97
Collin Jilbert <cjilbert504@gmail.com>
2019-11-12 15:56:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
-  array.each {|name| puts "#{name}"}
+  array.each_with_index {|item, index|
+  puts "#{index}. #{item}"}
 end
 
 def summon_captain_planet# code an argument here
